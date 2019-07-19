6693a014a7f5009d22579b9642935f942af2d817
Thayanne <thayannevls@gmail.com>
2019-07-19 18:17:55 -0300

    add offlinePage

---
+++ react/OfflinePage.js
@@ -0,0 +1,3 @@
+import OfflinePage from './components/OfflinePage'
+
+export default OfflinePage
---
+++ react/components/OfflinePage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'vtex.styleguide'
+
+const OfflinePage = () => {
+  <div>
+    <h1>You're Offline</h1>
+    <Button>Try again</Button>
+  </div>
+}
+
+export default OfflinePage
